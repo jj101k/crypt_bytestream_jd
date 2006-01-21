9d3dbf032722c0638fe84aec9d5ea075b134c038
Jim (encrypted) <jimcrypt@Jim-Driscolls-Computer.local>
2006-01-21 14:29:38 +0000

    Get yes/no responses from the tests

--- test.rb
+++ test.rb
@@ -1,6 +1,22 @@
 require "bytestream"
 foo=ByteStream.new("fooz")
-p foo
+puts "Checking integrity..."
+puts(if(foo=="fooz")
+	"Ok" 
+else 
+	"No" 
+end)
+puts "Checking XOR..."
 foo^="123"
-p foo
-p foo^"fooz"
+puts(if(foo=="W]\\z")
+	"Ok" 
+else 
+	"No" 
+end)
+puts "Checking that XOR won't de-pad..."
+foo^="fooz"
+puts(if(foo=="123\000")
+	"Ok" 
+else 
+	"No" 
+end)
