93fc7107301e6ba98612e7e64f028ca7a9e624c3
Jim Driscoll <jim.a.driscoll@gmail.com>
2023-08-19 12:02:48 +0100

    Minor style (whitespace) cleanup

--- install-pr.rb
+++ install-pr.rb
@@ -3,5 +3,5 @@ include RbConfig
 require "fileutils"
 include FileUtils::Verbose
 
-mkdir_p(CONFIG["sitelibdir"]+"/jdcrypt")
-install("bytestream.rb", CONFIG["sitelibdir"]+"/jdcrypt/", :mode=>0644)
+mkdir_p(CONFIG["sitelibdir"] + "/jdcrypt")
+install("bytestream.rb", CONFIG["sitelibdir"] + "/jdcrypt/", :mode=>0644)
--- test.rb
+++ test.rb
@@ -1,29 +1,29 @@
 require "./bytestream"
-foo=JdCrypt::ByteStream.new("fooz")
+foo = JdCrypt::ByteStream.new("fooz")
 puts "Checking integrity..."
-puts(if(foo=="fooz")
-	"Ok"
+puts(if(foo == "fooz")
+    "Ok"
 else
-	"No"
+    "No"
 end)
 puts "Checking XOR..."
-foo^="123"
-puts(if(foo=="W]\\z")
-	"Ok"
+foo ^= "123"
+puts(if(foo == "W]\\z")
+    "Ok"
 else
-	"No"
+    "No"
 end)
 puts "Checking that XOR won't de-pad..."
-foo^="fooz"
-puts(if(foo=="123\000")
-	"Ok"
+foo ^= "fooz"
+puts(if(foo == "123\000")
+    "Ok"
 else
-	"No"
+    "No"
 end)
 
-TestTimes=10000
+TestTimes = 10000
 puts "Testing performance: #{TestTimes} simple XORs"
 TestTimes.times do
   |i|
-  foo^=i.to_s
+  foo ^= i.to_s
 end
