afc3d7516565e58c62e57eb50fb43174c803ca87
Jim Driscoll <jim.a.driscoll@gmail.com>
2023-08-20 10:55:16 +0100

    Copy-paste reduction

--- test.rb
+++ test.rb
@@ -1,66 +1,39 @@
 require "./bytestream"
 foo = JdCrypt::ByteStream.new("fooz")
+
+def assertWarn(v)
+    puts(if v
+        "Ok"
+    else
+        "No"
+    end)
+end
+
 puts "Checking integrity..."
-puts(if(foo == "fooz")
-    "Ok"
-else
-    "No"
-end)
+assertWarn(foo == "fooz")
 puts "Checking + (32b)..."
-puts(if(foo + "    " == ["868F8F9A"].pack("H*"))
-    "Ok"
-else
-    "No"
-end)
+assertWarn(foo + "    " == ["868F8F9A"].pack("H*"))
 Foo = JdCrypt::ByteStream.new("FOOX")
 puts "Checking + (32b, long)..."
-puts(if(Foo + "        " == "foox    ")
-    "Ok"
-else
-    "No"
-end)
+assertWarn(Foo + "        " == "foox    ")
 puts "Checking + (8b)..."
-puts(if(foo + " " == ["866f6f7a"].pack("H*"))
-    "Ok"
-else
-    "No"
-end)
+assertWarn(foo + " " == ["866f6f7a"].pack("H*"))
 puts "Checking + (8b, long)..."
-puts(if(Foo + "     " == "foox ")
-    "Ok"
-else
-    "No"
-end)
+assertWarn(Foo + "     " == "foox ")
 puts "Checking XOR..."
 foo ^= "123"
-puts(if(foo == "W]\\z")
-    "Ok"
-else
-    "No"
-end)
+assertWarn(foo == "W]\\z")
 puts "Checking that XOR won't de-pad..."
 foo ^= "fooz"
-puts(if(foo == "123\000")
-    "Ok"
-else
-    "No"
-end)
+assertWarn(foo == "123\000")
 
 puts "Checking byte-at (assign)"
 bar = JdCrypt::ByteStream.new("bar")
 bar.byte_at 1, 32
-puts(if(bar == "b r")
-    "Ok"
-else
-    "No"
-end)
+assertWarn(bar == "b r")
 
 puts "Checking byte-at (read)"
-puts(if(bar.byte_at(0) == "b".ord)
-    "Ok"
-else
-    "No"
-end)
+assertWarn(bar.byte_at(0) == "b".ord)
 
 TestTimes = 10000
 puts "Testing performance: #{TestTimes} simple XORs"
